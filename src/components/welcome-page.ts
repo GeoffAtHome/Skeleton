6bf47bded91372645cbd8e8db0582c856a58af6d
GeoffAtHome <geoff@soord.org.uk>
2021-01-27 15:45:41 +0000

    Initial baseline skeleton

---
+++ src/components/welcome-page.ts
@@ -0,0 +1,43 @@
+/**
+@license
+Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+import { html, customElement, css } from 'lit-element';
+import { PageViewElement } from './page-view-element';
+
+// These are the shared styles needed by this element.
+import { SharedStyles } from './shared-styles';
+
+@customElement('welcome-page')
+export class WelcomePage extends PageViewElement {
+    static get styles() {
+        return [
+            SharedStyles,
+            css`
+            :host {
+                display: block;
+                padding: 10px;
+              }
+              `
+
+        ];
+    }
+
+    protected render() {
+        return html`
+        <h1>Initial app version v0.0.1</h1>
+        <p>App description</p>
+        <br>
+        <h1>Releases</h1>
+        <ul>
+        <li>v0.0.1 - Initial version</li>
+        </ul>
+    `;
+    }
+}
---
+++ stories/my-app.stories.ts
@@ -0,0 +1,30 @@
+import { html, TemplateResult } from 'lit-html';
+import '../src/my-app.js';
+
+export default {
+  title: 'MyApp',
+  component: 'my-app',
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+interface Story<T> {
+  (args: T): TemplateResult;
+  args?: Partial<T>;
+  argTypes?: Record<string, unknown>;
+}
+
+interface ArgTypes {
+  title?: string;
+  backgroundColor?: string;
+}
+
+const Template: Story<ArgTypes> = ({ title, backgroundColor = 'white' }: ArgTypes) => html`
+  <my-app style="--my-app-background-color: ${backgroundColor}" .title=${title}></my-app>
+`;
+
+export const App = Template.bind({});
+App.args = {
+  title: 'My app',
+};
